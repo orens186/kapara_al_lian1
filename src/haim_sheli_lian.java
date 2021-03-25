e1a3b20811f5423e50eea0697df13f896df34b9b
Lian <lianfi@campus.technion.ac.il>
2021-03-25 17:46:35 +0200

    מקווה שתצליח לעשות שפגט

--- src/haim_sheli_lian.java
+++ src/haim_sheli_lian.java
@@ -2,6 +2,7 @@ public class haim_sheli_lian {
     public static void main(String[] args) {
         int x = 3;
         int y = 5;
-        int z = x + y;
+        int z = x+8;
+        int t= 10;
     }
 }
